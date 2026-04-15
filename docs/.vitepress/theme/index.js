6fc41190387352a5a0a21f84dbe02930a50aed36
1029920178@qq.com <1029910278@qq.com>
2026-04-16 00:16:27 +0800

    Add image zoom for docs

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,32 @@
 import './custom.css';
 import DefaultTheme from 'vitepress/theme';
+import { inBrowser, onContentUpdated } from 'vitepress';
+
+const zoomSelector = '.vp-doc img:not(.no-zoom)';
+let zoomInstance;
+
+async function bindImageZoom() {
+  if (!inBrowser) {
+    return;
+  }
+
+  const { default: mediumZoom } = await import('medium-zoom');
+
+  if (!zoomInstance) {
+    zoomInstance = mediumZoom({
+      background: 'rgba(10, 14, 24, 0.84)',
+      margin: 32,
+    });
+  }
+
+  zoomInstance.detach();
+  zoomInstance.attach(document.querySelectorAll(zoomSelector));
+}
+
+if (inBrowser) {
+  onContentUpdated(() => {
+    void bindImageZoom();
+  });
+}
 
 export default DefaultTheme;
